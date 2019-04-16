69de346bf99178f398edbd286ca3e41b6a55ba8d
Safe870 <49463377+Safe870@users.noreply.github.com>
2019-04-16 09:52:22 -0400

    Update shaman.js

--- loc/classes/shaman.js
+++ loc/classes/shaman.js
@@ -10,10 +10,11 @@ export default {
     ko: '샤먼',
     pt: 'Xamã',
     nl: 'Sjamaan'
+    it: 'Sciamano'
   },
   // Class description, visible during character creation
   description: {
-    en: 'The Shaman\'s role is to support the group by healing the Tank and offering offensive Buffs or crippling Debuffs.',
+    en: 'The Shaman's role is to support the group by healing the Tank and offering offensive Buffs or crippling Debuffs.',
     se: 'Schamanens roll är att hjälpa sin grupp med att hela upp Tanken och använda sig utav sina offensiva Buffs eller förlamande Debuffs.',
     ru: 'Шаманы играют роль поддержки группы, излечивая Танка и преподнося усиливающие бонусы или наносящие урон отрицательные эффекты.',
     de: 'Die Rolle der Schamanen besteht darin, die Gruppe zu unterstützen, indem sie den Panzer heilen und beleidigende Buffs anbieten oder Debuffs verkrüppeln.',
@@ -22,5 +23,6 @@ export default {
     ko: '샤먼의 역할은 탱크를 치유하고 공세적 인 버프 또는 맹독적인 디버프를 제공하여 그룹을 지원하는 것입니다.',
     pt: 'O papel dos xamãs é apoiar o grupo, curando o Tanque e oferecendo Buffs ofensivos ou Debuffs incapacitantes.',
     nl: 'De rol van de Sjamaan is om de groep te steunen door de Tank te helen en aanvallende Buffs of verlammende Debuffs aan te bieden.'
+    it: 'Il ruolo dello Shaman è di supportare il gruppo curando il Tank e offrendo Buff offensive o Debuffs paralizzanti.'
   }
 }
